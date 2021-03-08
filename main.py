a3d7ac3cf7574c81599194262c38b5bfedf9b842
anthony <boulmieran@gmail.com>
2021-03-08 11:22:52 +0100

    lib updated

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from lib.norcb import *
 from skgeom.draw import draw
 from numpy import abs
+from copy import deepcopy
+from numba import jit
+
 
 def create_domain(minx, miny, maxx, maxy):
     p1 = sg.Point2(minx, miny)
@@ -16,39 +19,98 @@ def create_domain(minx, miny, maxx, maxy):
 
     return sg.Polygon(corners)
 
-from copy import deepcopy
+
 if __name__ == '__main__':
-    domain = create_domain(-2, -2, 2, 2)
-#
+    minx, miny, maxx, maxy = 0, 0, 2., 2.
+    domain = create_domain(minx, miny, maxx, maxy)
 
-    PE = 2
-    N  = 8192
+    PE = 64
+    N  = 2**12
 
-    rcb_imbalance   = []
-    norcb_imbalance = []
+    #__points     = np.zeros((N, 2))
+    #__points[:int(N/2), :]   = uniform_circle(int(N/2), cx=1,   cy=1,   R=0.5)
+    __points = uniform_circle(N, cx=1.0, cy=1.0)
 
-    __points     = np.zeros((N, 2))
-    __points[:int(N/2), :] = uniform_circle(int(N/2), cx=1,cy=1, R=0.5)
-    __points[int(N / 2):, :] = uniform_circle(int(N / 2), cx=-1, cy=0, R=0.5)
-    bias         = np.zeros((int(N/2), 2))
-    bias[:, 0]   = np.random.rand(int(N/2)) - 0.5
-    bias[:, 1]   = np.random.rand(int(N/2)) - 0.5
+    print(__points)
 
-    velocities   = np.random.rand(N, 2) - .5 #expansion_velocity(__points)
-    velocities[:int(N/2), :] = np.random.rand(int(N/2), 2) + bias
-    velocities[int(N/2):, :] = np.random.rand(int(N/2), 2)
+    #bias         = np.zeros((int(N/2), 2))
+    #bias[:, 0]   = np.random.rand(int(N/2)) - 0.5
+    #bias[:, 1]   = np.random.rand(int(N/2)) - 0.5
+
+    velocities   = expansion_velocity(__points, [1.0, 1.0]) #np.random.rand(N, 2) #expansion_velocity(__points)
+    #velocities[:int(N/2), :] = np.random.rand(int(N/2), 2) + bias
+    #velocities[int(N/2):, :] = np.random.rand(int(N/2), 2) + bias
+
+    rcb_velocities   = deepcopy(velocities)
+    norcb_velocities = deepcopy(velocities)
 
     average_vel  = np.mean(velocities, axis=0)
 
     rcb_points   = deepcopy(__points)
     norcb_points = deepcopy(__points)
 
-    rcb   = partitioning_rcb(None, PE, rcb_points,    velocities, i=1, depth=0)
-    plt.figure()
-    ax = plt.gca()
-    norcb =     partitioning(ax, PE, norcb_points,  velocities, domain=domain, i=1, depth=0)
-    plt.show()
+    rcb   = partitioning_rcb(None, PE, rcb_points,    velocities, domain=domain, i=1, depth=0)
+    norcb =     partitioning(plt, PE, norcb_points,  velocities, domain=domain, i=1, depth=0)
     p0bis = norcb[0][0]
+
+    plt.show()
+    dt = 2e-3
+    NITER = 100
+    rcb_imbalance   = []
+    U_rcb = [0] * NITER
+    U_norcb = [0] * NITER
+    norcb_imbalance = []
+
+    points = __points
+    for i in range(NITER):
+        W = eval(norcb, points) * 1e-6
+        derose = (np.max(W) - np.mean(W))
+        U_norcb[i] = (np.max(W) - np.mean(W)) if i == 0 else U_norcb[i-1] + (np.max(W) - np.mean(W))
+        norcb_imbalance.append(compute_imbalance(W))
+
+        W = eval(rcb, points) * 1e-6
+        derose = (np.max(W) - np.mean(W))
+        U_rcb[i] = (np.max(W) - np.mean(W)) if i == 0 else U_rcb[i - 1] + (np.max(W) - np.mean(W))
+        rcb_imbalance.append(compute_imbalance(W))
+        points, velocities = move(minx, miny, maxx, maxy, points, velocities, dt)
+
+        fig, ax = plt.subplots(2, 1, figsize=(5, 5))
+        show_partitioned_points(ax[0], norcb,   points)
+        show_partitioned_points(ax[1],   rcb,   points)
+        ax[0].set_xlim(minx, maxx)
+        ax[1].set_xlim(minx, maxx)
+        ax[0].set_ylim(miny, maxy)
+        ax[1].set_ylim(miny, maxy)
+        ax[0].set_aspect('equal', 'box')
+        ax[1].set_aspect('equal', 'box')
+        fig.tight_layout()
+        plt.savefig(f"img-{i:03d}.png", dpi=300)
+        plt.close()
+
+    print(U_rcb, U_norcb)
+
+    plt.figure()
+    plt.title("Imbalance over time")
+    plt.xlabel("Imbalance metric")
+    plt.ylabel("Iteration")
+    plt.plot(rcb_imbalance, label='rcb')
+    plt.plot(norcb_imbalance, label='norcb')
+    plt.legend()
+    plt.tight_layout()
+    plt.show()
+    plt.close()
+
+    plt.figure()
+    plt.title("U over time")
+    plt.xlabel("U")
+    plt.ylabel("Iteration")
+    plt.plot(U_rcb, label='rcb')
+    plt.plot(U_norcb, label='norcb')
+    plt.legend()
+    plt.tight_layout()
+    plt.show()
+    plt.close()
+
 
 #     intersections = []
 #     for i, bis1 in enumerate(p0bis):
